b02603ac0a99784834c2609ed48e07f7d3a15052
vihahuynh <huynhviha1703@gmail.com>
2021-11-22 18:35:01 +0700

    fetch meals from firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,41 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        "https://react-http-ced21-default-rtdb.asia-southeast1.firebasedatabase.app/meals"
+      );
+
+      if (!response.ok) throw new Error("Something went wrong!");
+
+      const data = await response.json();
+      let loadedMeals = [];
+
+      for (const key of Object.keys(data)) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+    fetchMeals().catch((error) => {
+      setHttpError(error.message);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles["meals-loading"]}>
+        <p>Loading...</p>;
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles["meals-error"]}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
   return (
     <section className={styles.meals}>
       <Card>
         <ul>
-          {DUMMY_MEALS.map((meal) => (
+          {meals.map((meal) => (
             <MealItem key={meal.id} meal={meal} />
           ))}
         </ul>
